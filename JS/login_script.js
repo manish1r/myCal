d67c29143a2802bc6e7c57f76addb03b5b64d1be
Manish Rathod <23h51a05g8@cmrcet.ac.in>
2025-12-16 21:24:59 +0530

    Update login_script.js

--- JS/login_script.js
+++ JS/login_script.js
@@ -13,7 +13,7 @@ button.addEventListener("click",async()=>{
     }
     let found=false;
     try{
-        const response=await fetch("http://localhost:3030/login",{
+        const response=await fetch("https://mycal-jgvb.onrender.com/login",{
             method:"POST",
             headers:{"Content-Type":"application/json"},
             body:JSON.stringify({username:user,password:password})
